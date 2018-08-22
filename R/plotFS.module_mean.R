a7b2ad3b4e2b9c984d191502e78c1d895df4766f
MartinWut <martinwutke@hotmail.de>
2018-08-23 01:09:52 +0200

    updating the plotFS function

--- R/plotFS.module_mean.R
+++ R/plotFS.module_mean.R
@@ -1,14 +1,14 @@
-#' plotFS.fac_mean
+#' plotFS.module_mean
 #'
-#' @param x Object of the fac_mean
+#' @param x Object of the module_mean
 #' @export
 
-plotFS.fac_mean <- function(x){
+plotFS.module_mean <- function(x){
   df <- data.frame(x[[1]], x[[2]])
   ggplot(df, aes(x = x..1.., y = x..1.., fill=x..2..))+
     geom_bar(stat = "identity")+
-    xlab("Faculty") + ylab("Mean grades") +
+    xlab("Module") + ylab("Mean grades") +
     coord_cartesian(ylim=c(min(df$x..1..-0.5),max(df$x..1..)+0.5)) +
     guides(fill=guide_legend(title=NULL)) +
-    ggtitle("Faculty Mean")
+    theme( axis.text.x = element_blank())
 }
