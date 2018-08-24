bd5319e67507832e4f5aba3c8d158d23dbc2acca
MartinWut <martinwutke@hotmail.de>
2018-08-24 16:43:06 +0200

    -

--- R/examiner_compare.R
+++ R/examiner_compare.R
@@ -10,7 +10,6 @@
 #' @param semester_vector A character vector vector containing the semester names in the form "WSYY/YY" for winter semester and "SoSeYY" for summer semester (e.g. semester_vector <- c("WS17/18", "SoSe18")). By default all semesters are considered.
 #' @param download Logical. If TRUE the corresponding data is downloaded and used for computing the examiner mean values. If False the faculty data has to be provided using the function faculty_down in the first place.
 #' @param FacData FacData A List containing the faculty or module data. Typically produced by the function faculty_down.
-#' @return The Return is an object of class "examiner_compare" and contains the names of the examiners and their corresponding grade mean values for the model over the chosen semesters.
 #' @examples # Compute the mean values for the different examiners for the introductory course in statistics at the economic faculty over all semesters.
 #'
 #' faculty_data() # the required faculty-number is 12
