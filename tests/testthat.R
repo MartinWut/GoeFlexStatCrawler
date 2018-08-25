13bde2ce222019f905b57edca180d43ce2129498
Simone Gerle <gerle.s@freenet.de>
2018-08-25 11:58:23 +0200

    test_that

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,5 +2,5 @@ library(testthat)
 library(GoeFlexStatCrawler)
 
 #test_check("GoeFlexStatCrawler")
-#test_dir("tests/testthat")
-auto_test("R", "tests/testthat")
+test_dir("tests/testthat")
+#auto_test("R", "tests/testthat")
